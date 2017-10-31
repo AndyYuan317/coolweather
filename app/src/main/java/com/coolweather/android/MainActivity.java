52b6034fe8af72c20ad877186d768df15906e5e0
AndyYuan317 <1065416114@qq.com>
2017-10-31 22:11:39 +0800

    First commit.

---
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -0,0 +1,13 @@
+package com.coolweather.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//https://github.com/AndyYuan317/coolweather.git
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
